7735de1ed1ba17a5fcbbe634600cac2e75ae746c
kaanelloed <kaanelloed@proton.me>
2023-09-09 10:10:36 -0400

    feat: add compose libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,16 +32,20 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.jaredrummler:colorpicker:1.1.0")
     implementation("io.github.reandroid:ARSCLib:1.2.4")
@@ -49,6 +53,19 @@ dependencies {
     implementation("com.android.tools.build:apksig:8.1.1")
     implementation("com.caverock:androidsvg:1.4")
 
+
+    //compose
+    val composeBom = platform("androidx.compose:compose-bom:2023.09.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+
+    //Test
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
