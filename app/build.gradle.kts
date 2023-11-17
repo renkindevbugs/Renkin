6af8d3f21fb40baa35af4fca4a8a61cd35668fc5
kaanelloed <kaanelloed@proton.me>
2023-11-17 17:02:45 -0500

    feat: update gradle, agp, kotlin and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
 }
 
@@ -46,11 +46,11 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("io.github.reandroid:ARSCLib:1.2.4")
     implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
-    implementation("com.android.tools.build:apksig:8.1.3")
+    implementation("com.android.tools.build:apksig:8.1.4")
     implementation("com.caverock:androidsvg:1.4")
 
     //compose
-    val composeBom = platform("androidx.compose:compose-bom:2023.09.00")
+    val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
     implementation("androidx.compose.material3:material3:1.1.2")
