41cc980afc36f37b32b21e25186437f0ea6ad955
kaanelloed <kaanelloed@proton.me>
2023-11-30 09:29:16 -0500

    style: add version in settings

--- app/src/main/java/com/kaanelloed/iconeration/ui/Settings.kt
+++ app/src/main/java/com/kaanelloed/iconeration/ui/Settings.kt
@@ -1,8 +1,13 @@
 package com.kaanelloed.iconeration.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -17,9 +22,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import com.kaanelloed.iconeration.BuildConfig
 import com.kaanelloed.iconeration.data.DarkMode
 import com.kaanelloed.iconeration.data.DarkModeLabels
 import com.kaanelloed.iconeration.data.getDarkModeValue
@@ -35,7 +43,10 @@ fun SettingsDialog(prefs: DataStore<Preferences>, onDismiss: (() -> Unit)) {
         onDismissRequest = onDismiss,
         title = { Text("Settings") },
         text = {
-            DarkModeDropdown(prefs)
+            Column {
+                DarkModeDropdown(prefs)
+                AppVersion()
+            }
         },
         confirmButton = {}
     )
@@ -88,5 +99,17 @@ fun DarkModeDropdown(prefs: DataStore<Preferences>) {
                 )
             }
         }
+    }
+}
+
+@Composable
+fun AppVersion() {
+    Divider(modifier = Modifier.fillMaxWidth().padding(top = 4.dp), thickness = Dp.Hairline, color = MaterialTheme.colorScheme.outline)
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.End) {
+        Text(
+            text = "Version " + BuildConfig.VERSION_NAME,
+            fontSize = 12.sp
+        )
     }
 }
