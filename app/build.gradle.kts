da9f1a7e083fdd1e9999f486b218ba8810014840
kaanelloed <kaanelloed@proton.me>
2023-12-18 11:51:58 -0500

    build: update kotlin, ksp, compose compiler and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,21 +46,21 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
+        kotlinCompilerExtensionVersion = "1.5.6"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     //Apk related
     implementation("io.github.reandroid:ARSCLib:1.2.4")
     implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
     implementation("com.android.tools.build:apksig:8.2.0")
-    implementation("ru.solrudev.ackpine:ackpine-core:0.3.2")
-    implementation("ru.solrudev.ackpine:ackpine-ktx:0.3.2")
+    implementation("ru.solrudev.ackpine:ackpine-core:0.4.0")
+    implementation("ru.solrudev.ackpine:ackpine-ktx:0.4.0")
 
     //Compose
     val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
@@ -71,9 +71,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("io.coil-kt:coil-compose:2.5.0")
-    implementation("com.github.skydoves:colorpicker-compose:1.0.6")
+    implementation("com.github.skydoves:colorpicker-compose:1.0.7")
 
     //Data
     implementation("androidx.room:room-ktx:2.6.1")
