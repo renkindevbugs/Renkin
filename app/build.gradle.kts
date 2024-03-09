7f9a9d2e1408cfe4054d165f84d11f6ad1013a4b
kaanelloed <kaanelloed@proton.me>
2024-03-09 09:32:32 -0500

    build: bump version to 1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kaanelloed.iconeration"
         minSdk = 26
         targetSdk = 34
-        versionCode = 26
-        versionName = "1.4.1"
+        versionCode = 27
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
