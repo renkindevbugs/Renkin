2ca454c88422831b03954f9169d16ab2dfd19f50
kaanelloed <kaanelloed@proton.me>
2024-12-16 10:59:00 -0500

    build: bump vesion to 2024.12.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kaanelloed.iconeration"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 38
-        versionName = "2024.12.00"
+        versionCode = 39
+        versionName = "2024.12.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
