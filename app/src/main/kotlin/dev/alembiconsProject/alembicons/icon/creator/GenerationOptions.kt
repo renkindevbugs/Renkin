1fe2d381752b32fe9c615b3c40416c2b60bcb382
renkindevbugs <renkin.dev.bugs@gmail.com>
2026-06-13 11:37:19 +0200

    refactor: make GenerationOptions a data class
    Drops the hand-written equals/hashCode that had to be updated by hand
    for every new field (the edge params were a recent example). The data
    class generates them from the primary constructor instead.

--- app/src/main/kotlin/dev/alembiconsProject/alembicons/icon/creator/GenerationOptions.kt
+++ app/src/main/kotlin/dev/alembiconsProject/alembicons/icon/creator/GenerationOptions.kt
@@ -4,7 +4,7 @@ import dev.alembiconsProject.alembicons.data.ImageEdit
 import dev.alembiconsProject.alembicons.data.Source
 import dev.alembiconsProject.alembicons.data.TextType
 
-class GenerationOptions(
+data class GenerationOptions(
     val primarySource: Source,
     val primaryImageEdit: ImageEdit,
     val primaryTextType: TextType,
@@ -60,54 +60,4 @@ class GenerationOptions(
         edgeGaussianRadius,
         edgeContrastNormalized
     )
-
-    override fun hashCode(): Int {
-        var result = primarySource.hashCode()
-        result = 31 * result + primaryImageEdit.hashCode()
-        result = 31 * result + primaryTextType.hashCode()
-        result = 31 * result + primaryIconPack.hashCode()
-        result = 31 * result + secondarySource.hashCode()
-        result = 31 * result + secondaryImageEdit.hashCode()
-        result = 31 * result + secondaryTextType.hashCode()
-        result = 31 * result + secondaryIconPack.hashCode()
-        result = 31 * result + color
-        result = 31 * result + bgColor
-        result = 31 * result + vector.hashCode()
-        result = 31 * result + monochrome.hashCode()
-        result = 31 * result + themed.hashCode()
-        result = 31 * result + override.hashCode()
-        result = 31 * result + edgeLowThreshold.hashCode()
-        result = 31 * result + edgeHighThreshold.hashCode()
-        result = 31 * result + edgeGaussianRadius.hashCode()
-        result = 31 * result + edgeContrastNormalized.hashCode()
-        return result
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as GenerationOptions
-
-        if (primarySource != other.primarySource) return false
-        if (primaryImageEdit != other.primaryImageEdit) return false
-        if (primaryTextType != other.primaryTextType) return false
-        if (primaryIconPack != other.primaryIconPack) return false
-        if (secondarySource != other.secondarySource) return false
-        if (secondaryImageEdit != other.secondaryImageEdit) return false
-        if (secondaryTextType != other.secondaryTextType) return false
-        if (secondaryIconPack != other.secondaryIconPack) return false
-        if (color != other.color) return false
-        if (bgColor != other.bgColor) return false
-        if (vector != other.vector) return false
-        if (monochrome != other.monochrome) return false
-        if (themed != other.themed) return false
-        if (override != other.override) return false
-        if (edgeLowThreshold != other.edgeLowThreshold) return false
-        if (edgeHighThreshold != other.edgeHighThreshold) return false
-        if (edgeGaussianRadius != other.edgeGaussianRadius) return false
-        if (edgeContrastNormalized != other.edgeContrastNormalized) return false
-
-        return true
-    }
 }
